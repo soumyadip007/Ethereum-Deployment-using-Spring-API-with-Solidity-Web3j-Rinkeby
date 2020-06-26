2fb2050b9d285743de9ea0542fe6adabab7cec2e
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-27 01:18:56 +0530

    Application

--- EthereumJava/src/main/java/com/one/spring/ethereum/Blockchain/Application.java
+++ EthereumJava/src/main/java/com/one/spring/ethereum/Blockchain/Application.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
@@ -43,15 +45,17 @@ import com.one.spring.ethereum.Model.Greeter;
  *
  * <p>For further background information, refer to the project README.
  */
+
+//@Configuration
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) throws Exception {
-        new Application().run();
-    }
+//    public static void main(String[] args) throws Exception {
+//        new Application().run();
+//    }
 
-    private void run() throws Exception {
+    public static void run() throws Exception {
 
         // We start by creating a new web3j instance to connect to remote nodes on the network.
         // Note: if using web3j Android, use Web3jFactory.build(...
@@ -97,6 +101,69 @@ public class Application {
 
         // Lets modify the value in our smart contract
         TransactionReceipt transactionReceipt = contract.newGreeting(Data.data).send();
+
+        log.info("New value stored in remote smart contract: " + contract.greet().send());
+
+        // Events enable us to log specific events happening during the execution of our smart
+        // contract to the blockchain. Index events cannot be logged in their entirety.
+        // For Strings and arrays, the hash of values is provided, not the original value.
+        // For further information, refer to https://docs.web3j.io/filters.html#filters-and-events
+        for (Greeter.ModifiedEventResponse event : contract.getModifiedEvents(transactionReceipt)) {
+            log.info("Modify event fired, previous value: " + event.oldGreeting
+                    + ", new value: " + event.newGreeting);
+            log.info("Indexed event previous value: " + Numeric.toHexString(event.oldGreetingIdx)
+                    + ", new value: " + Numeric.toHexString(event.newGreetingIdx));
+        }
+    }
+    
+    
+    
+    public static void runWithValue(String value) throws Exception {
+
+        // We start by creating a new web3j instance to connect to remote nodes on the network.
+        // Note: if using web3j Android, use Web3jFactory.build(...
+        Web3j web3j = Web3j.build(new HttpService(
+                "https://rinkeby.infura.io/v3/656923d9738f4d4fbb36a0f3e2f6d89f"));  // FIXME: Enter your Infura token here;
+        log.info("Connected to Ethereum client version: "
+                + web3j.web3ClientVersion().send().getWeb3ClientVersion());
+
+        // We then need to load our Ethereum wallet file
+        // FIXME: Generate a new wallet file using the web3j command line tools https://docs.web3j.io/command_line.html
+        Credentials credentials =
+                WalletUtils.loadCredentials(
+                        "12341234",
+                        "C:\\Users\\Soumyadip\\AppData\\Roaming\\Ethereum\\keystore\\UTC--2020-06-25T14-33-07.307305900Z--fa19f9435b1b3a884f751c53e4a02c4b27dc9a61");
+        log.info("Credentials loaded");
+
+        // FIXME: Request some Ether for the Rinkeby test network at https://www.rinkeby.io/#faucet
+        log.info("Sending 1 Wei ("
+                + Convert.fromWei("1", Convert.Unit.ETHER).toPlainString() + " Ether)");
+        TransactionReceipt transferReceipt = Transfer.sendFunds(
+                web3j, credentials,
+                "0x3FAdD7053708b93a1d922523B90498Cc392C4758",  // you can put any address here
+                BigDecimal.ONE, Convert.Unit.WEI)  // 1 wei = 10^-18 Ether
+                .send();
+        log.info("Transaction complete, view it at https://rinkeby.etherscan.io/tx/"
+                + transferReceipt.getTransactionHash());
+
+        // Now lets deploy a smart contract
+        log.info("Deploying smart contract");
+        ContractGasProvider contractGasProvider = new DefaultGasProvider();
+        Greeter contract = Greeter.deploy(
+                web3j,
+                credentials,
+                contractGasProvider,
+                "test"
+        ).send();
+
+        String contractAddress = contract.getContractAddress();
+        log.info("Smart contract deployed to address " + contractAddress);
+        log.info("View contract at https://rinkeby.etherscan.io/address/" + contractAddress);
+
+        log.info("Value stored in remote smart contract: " + contract.greet().send());
+
+        // Lets modify the value in our smart contract
+        TransactionReceipt transactionReceipt = contract.newGreeting(value).send();
 
         log.info("New value stored in remote smart contract: " + contract.greet().send());
 
