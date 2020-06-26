1ed88144f440183dd73593864d92f8a2d55ca0ae
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-27 00:42:20 +0530

    Data

---
+++ EthereumJava/src/main/java/com/one/spring/ethereum/Blockchain/Data.java
@@ -0,0 +1,98 @@
+package com.one.spring.ethereum.Blockchain;
+
+public class Data {
+
+	public static String data="Cooperators DP 37.0\r\n" + 
+			"Isolators DP 19.0\r\n" + 
+			"Exploiters DP 13.0\r\n" + 
+			"Converting Exploiters45 to Cooperators\r\n" + 
+			"Converting Exploiters46 to Cooperators\r\n" + 
+			"Converting Exploiters47 to Cooperators\r\n" + 
+			"Converting Exploiters48 to Cooperators\r\n" + 
+			"Converting Exploiters49 to Cooperators\r\n" + 
+			"Converting Exploiters50 to Cooperators\r\n" + 
+			"Converting Exploiters51 to Cooperators\r\n" + 
+			"Converting Exploiters52 to Cooperators\r\n" + 
+			"Converting Exploiters53 to Cooperators\r\n" + 
+			"Converting Exploiters54 to Cooperators\r\n" + 
+			"Converting Exploiters55 to Cooperators\r\n" + 
+			"Converting Exploiters56 to Cooperators\r\n" + 
+			"Converting Exploiters57 to Cooperators\r\n" + 
+			"Converting Exploiters58 to Cooperators\r\n" + 
+			"Converting Exploiters59 to Cooperators\r\n" + 
+			"Converting Exploiters60 to Cooperators\r\n" + 
+			"Converting Exploiters61 to Cooperators\r\n" + 
+			"Converting Exploiters62 to Cooperators\r\n" + 
+			"Converting Exploiters63 to Cooperators\r\n" + 
+			"Converting Exploiters64 to Cooperators\r\n" + 
+			"Converting Exploiters65 to Cooperators\r\n" + 
+			"Converting Exploiters66 to Cooperators\r\n" + 
+			"Converting Exploiters67 to Cooperators\r\n" + 
+			"Converting Exploiters68 to Cooperators\r\n" + 
+			"Converting Exploiters69 to Cooperators\r\n" + 
+			"Converting Exploiters70 to Cooperators\r\n" + 
+			"Converting Exploiters71 to Cooperators\r\n" + 
+			"Converting Exploiters72 to Cooperators\r\n" + 
+			"Converting Exploiters73 to Cooperators\r\n" + 
+			"Converting Exploiters74 to Cooperators\r\n" + 
+			"Converting Exploiters75 to Cooperators\r\n" + 
+			"Converting Exploiters76 to Cooperators\r\n" + 
+			"Converting Exploiters77 to Cooperators\r\n" + 
+			"Converting Exploiters78 to Cooperators\r\n" + 
+			"Converting Exploiters79 to Cooperators\r\n" + 
+			"Converting Exploiters80 to Cooperators\r\n" + 
+			"Converting Exploiters81 to Cooperators\r\n" + 
+			"Converting Exploiters82 to Cooperators\r\n" + 
+			"Converting Exploiters83 to Cooperators\r\n" + 
+			"Converting Exploiters84 to Cooperators\r\n" + 
+			"Converting Exploiters85 to Cooperators\r\n" + 
+			"Converting Exploiters86 to Cooperators\r\n" + 
+			"Converting Exploiters87 to Cooperators\r\n" + 
+			"Converting Exploiters88 to Cooperators\r\n" + 
+			"Converting Exploiters89 to Cooperators\r\n" + 
+			"Converting Isolators90 to Cooperators\r\n" + 
+			"Converting Isolators91 to Cooperators\r\n" + 
+			"Converting Isolators92 to Cooperators\r\n" + 
+			"Converting Isolators93 to Cooperators\r\n" + 
+			"Converting Isolators94 to Cooperators\r\n" + 
+			"Converting Isolators95 to Cooperators\r\n" + 
+			"Converting Isolators96 to Cooperators\r\n" + 
+			"Converting Isolators97 to Cooperators\r\n" + 
+			"Converting Isolators98 to Cooperators\r\n" + 
+			"Converting Isolators99 to Cooperators\r\n" + 
+			"Converting Isolators100 to Cooperators\r\n" + 
+			"Converting Isolators101 to Cooperators\r\n" + 
+			"Converting Isolators102 to Cooperators\r\n" + 
+			"Converting Isolators103 to Cooperators\r\n" + 
+			"Converting Isolators104 to Cooperators\r\n" + 
+			"Converting Isolators105 to Cooperators\r\n" + 
+			"Converting Isolators106 to Cooperators\r\n" + 
+			"Converting Isolators107 to Cooperators\r\n" + 
+			"Converting Isolators108 to Cooperators\r\n" + 
+			"Converting Isolators109 to Cooperators\r\n" + 
+			"Converting Isolators110 to Cooperators\r\n" + 
+			"Converting Isolators111 to Cooperators\r\n" + 
+			"Converting Isolators112 to Cooperators\r\n" + 
+			"Converting Isolators113 to Cooperators\r\n" + 
+			"Converting Isolators114 to Cooperators\r\n" + 
+			"Converting Isolators115 to Cooperators\r\n" + 
+			"Converting Isolators116 to Cooperators\r\n" + 
+			"Converting Isolators117 to Cooperators\r\n" + 
+			"Converting Isolators118 to Cooperators\r\n" + 
+			"Converting Isolators119 to Cooperators\r\n" + 
+			"Converting Isolators120 to Cooperators\r\n" + 
+			"Converting Isolators121 to Cooperators\r\n" + 
+			"Converting Isolators122 to Cooperators\r\n" + 
+			"Converting Isolators123 to Cooperators\r\n" + 
+			"Converting Isolators124 to Cooperators\r\n" + 
+			"Converting Isolators125 to Cooperators\r\n" + 
+			"Converting Isolators126 to Cooperators\r\n" + 
+			"Converting Isolators127 to Cooperators\r\n" + 
+			"Converting Isolators128 to Cooperators\r\n" + 
+			"Converting Isolators129 to Cooperators\r\n" + 
+			"Converting Isolators130 to Cooperators\r\n" + 
+			"Converting Isolators131 to Cooperators\r\n" + 
+			"Converting Isolators132 to Cooperators\r\n" + 
+			"Converting Isolators133 to Cooperators\r\n" + 
+			"Converting Isolators134 to Cooperators";
+}
