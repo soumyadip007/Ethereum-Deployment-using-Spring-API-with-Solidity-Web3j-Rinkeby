3331f7772a0ea5453cb7cad5062bc9d109c0e927
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-07-02 19:51:04 +0530

    Ethereum

--- EthereumJava/src/main/java/com/one/spring/ethereum/Controller/EthereumController.java
+++ EthereumJava/src/main/java/com/one/spring/ethereum/Controller/EthereumController.java
@@ -2,9 +2,13 @@ package com.one.spring.ethereum.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.one.spring.ethereum.Blockchain.Application;
+import com.one.spring.ethereum.Blockchain.Data;
+import com.one.spring.ethereum.Model.Block;
 
 @RestController
 public class EthereumController {
@@ -24,7 +28,26 @@ public class EthereumController {
 	@GetMapping("/blockchain-value/{value}")
 	public void TestAPIValue( @PathVariable String value) throws Exception {
 		
-		Application.runWithValue(value);
 		
+		System.out.println(value);
+	//	Application.runWithValue();
+		
+	}
+	
+	@PostMapping("/blockchain-post")
+	public void TestAPIPostValue( @RequestBody Block block) throws Exception {
+		
+		System.out.print(block.data);
+		System.out.print(block.data);
+		System.out.print(block.data);
+		System.out.print(block.data);
+		System.out.print(block.data);
+		System.out.print(Data.data);
+		
+		Data.data=block.data;
+		System.out.print(Data.data);
+		
+		
+		//Application.run();
 	}
 }
